edc09423db016ebd442e348f5cb0201bf42c7dbe
dllo <1658666520@qq.com>
2016-10-17 15:29:01 +0800

    创建工程

---
+++ Testllll/app/src/main/java/com/maqianyu/testllll/Bean/Bean.java
@@ -0,0 +1,7 @@
+package com.maqianyu.testllll.Bean;
+
+/**
+ * Created by dllo on 16/10/17.
+ */
+public class Bean {
+}
---
+++ Testllll/app/src/main/java/com/maqianyu/testllll/Z/Z.java
@@ -0,0 +1,7 @@
+package com.maqianyu.testllll.Z;
+
+/**
+ * Created by dllo on 16/10/17.
+ */
+public class Z {
+}
