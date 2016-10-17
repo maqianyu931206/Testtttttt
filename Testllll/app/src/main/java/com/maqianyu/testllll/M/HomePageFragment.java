97a551027cce2257edf95677ace0fc1f1beaf3c1
qiansha <pangmeilnyx@163.com>
2016-10-17 15:34:30 +0800

    加了Fragment

---
+++ Testllll/app/src/main/java/com/maqianyu/testllll/M/HomePageFragment.java
@@ -0,0 +1,29 @@
+package com.maqianyu.testllll.M;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by dllo on 16/10/17.
+ */
+public class HomePageFragment extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+}
